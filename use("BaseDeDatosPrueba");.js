53ccc7cd77bcab61b61b1e37f2b91ac6390ff7c7
Noche199 <mendoza.perez.samuel.46@cbtis118.edu.mx>
2025-11-19 17:50:23 +0000

    agrego las colecciones

---
+++ use("BaseDeDatosPrueba");.js
@@ -0,0 +1,7 @@
+use("BaseDeDatosPrueba");
+
+db.createCollection("Carros");
+db.createCollection("Motos");
+db.createCollection("Perros");
+db.createCollection("Gatos");
+db.createCollection("Bebidas");
