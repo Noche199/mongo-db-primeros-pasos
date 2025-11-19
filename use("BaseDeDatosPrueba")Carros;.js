2426e6ad98c12bffb77da579a5bbfc59d4f805de
Noche199 <mendoza.perez.samuel.46@cbtis118.edu.mx>
2025-11-19 19:17:35 +0000

    añadir a las clases

---
+++ use("BaseDeDatosPrueba")Carros;.js
@@ -0,0 +1,52 @@
+use("BaseDeDatosPrueba");
+db.Carros.insertMany([{
+ Marca: "Mitsubishi",
+ Modelo: "Lancer",
+ año: "2015",
+},
+{
+ Marca: "Nissan",
+ Modelo: "GTR34",
+ año: "1999",
+},
+{
+ Marca: "Nissan",
+ Modelo: "350Z",
+ año: "2006",
+},
+{
+ Marca: "Toyota",
+ Modelo: "Trueno ae86",
+ año: "1983",
+},
+{
+ Marca: "Mitsubishi",
+ Modelo: "Eclipse",
+ año: "1995",
+},
+{
+ Marca: "Honda",
+ Modelo: "NSX",
+ año: "1990",
+},
+{
+ Marca: "Honda",
+ Modelo: "Civic type r",
+ año: "2018",
+},
+{
+ Marca: "BMW",
+ Modelo: "M3gtr",
+ año: "1983",
+},
+{
+ Marca: "Mazda",
+ Modelo: "RX7",
+ año: "1999",
+},
+{
+ Marca: "Mazda",
+ Modelo: "MX5",
+ año: "2000",
+},
+])
---
+++ use("BaseDeDatosPrueba")Motos;.js
@@ -0,0 +1,53 @@
+use("BaseDeDatosPrueba");
+db.Motos.insertMany([
+  {
+    "Marca": "Yamaha",
+    "Modelo": "YZF-R6",
+    "año": "2017"
+  },
+  {
+    "Marca": "Kawasaki",
+    "Modelo": "Ninja ZX-6R",
+    "año": "2020"
+  },
+  {
+    "Marca": "Honda",
+    "Modelo": "CBR1000RR",
+    "año": "2019"
+  },
+  {
+    "Marca": "Suzuki",
+    "Modelo": "GSX-R750",
+    "año": "2018"
+  },
+  {
+    "Marca": "Ducati",
+    "Modelo": "Panigale V4",
+    "año": "2021"
+  },
+  {
+    "Marca": "BMW",
+    "Modelo": "S1000RR",
+    "año": "2022"
+  },
+  {
+    "Marca": "Aprilia",
+    "Modelo": "RSV4",
+    "año": "2020"
+  },
+  {
+    "Marca": "Triumph",
+    "Modelo": "Daytona 765",
+    "año": "2023"
+  },
+  {
+    "Marca": "MV Agusta",
+    "Modelo": "F4",
+    "año": "2019"
+  },
+  {
+    "Marca": "KTM",
+    "Modelo": "RC 390",
+    "año": "2022"
+  }
+])
